80cbd7b01e1631741be3c2b7b569c24c9c4e3e51
Robert Pace <rpace1994@gmail.com>
2019-06-29 08:55:19 -0500

    Dice files added the wrong way

---
+++ ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Rob Pace on 6/10/19.
+//  Copyright © 2019 Rob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+    updateDiceImages()
+    
+    }
+    
+
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+           
+    }
+    
+    func updateDiceImages() {
+        
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randomDiceIndex1)
+        
+        diceImageView1.image = UIImage (named: diceArray[randomDiceIndex1])
+        
+        diceImageView2.image = UIImage (named: diceArray[randomDiceIndex2])
+        
+    }
+    
+}
+
